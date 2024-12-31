40279c215934a7878cfc8b0d04f1d612a3392c6f
xia__mc <2052472631@qq.com>
2024-12-31 18:48:24 +0800

    update to 3.0

--- src/main/java/keystrokesmod/Const.java
+++ src/main/java/keystrokesmod/Const.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Unmodifiable
 public final class Const {
     public static final String NAME = "Raven XD";
-    public static final String VERSION = "Developing build";
+    public static final String VERSION = "3.0";
     public static final List<String> CHANGELOG = Collections.unmodifiableList(Arrays.asList(
             // Add
             "[+] **Add** Optimize.",

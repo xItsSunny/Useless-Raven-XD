27b8ba6fbd5994601c4847105ca9370c4d7e8de4
xItsSunny <luk.kliem@gmail.com>
2025-01-20 14:49:01 +0100

    added e-sound cape

--- src/main/java/keystrokesmod/module/impl/render/CustomCape.java
+++ src/main/java/keystrokesmod/module/impl/render/CustomCape.java
@@ -24,7 +24,8 @@ import static com.mojang.realmsclient.gui.ChatFormatting.*;
 public final class CustomCape extends Module {
     public static final List<ResourceLocation> LOADED_CAPES = new ArrayList<>();
     public static String[] CAPES_NAME = new String[]{
-            "RavenAnime", "Cherry", "Die", "Astolfo"
+            "RavenAnime", "RavenXD", "RavenAqua", "RavenGreen", "RavenPurple", "RavenRed", "RavenWhite", "RavenYellow",
+            "Cherry", "Die", "Astolfo", "E-Sound"
     };
     public static final ModeSetting cape = new ModeSetting("Cape", CAPES_NAME, 0);
     private static File directory;
@@ -64,9 +65,8 @@ public final class CustomCape extends Module {
         }
 
         final String[] builtinCapes = new String[]{
-                "RavenAnime", "RavenAqua", "RavenGreen", "RavenPurple", "RavenRed", "RavenWhite", "RavenYellow",
-                "Cherry", "Die",
-                "Astolfo"
+                "RavenAnime", "RavenXD", "RavenAqua", "RavenGreen", "RavenPurple", "RavenRed", "RavenWhite", "RavenYellow",
+                "Cherry", "Die", "Astolfo", "E-Sound"
         };
         CAPES_NAME = new String[files.length + builtinCapes.length];
         LOADED_CAPES.clear();

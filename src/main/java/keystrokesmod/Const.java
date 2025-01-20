42848040e2a1e7ff7e9d7b55c9e2b5b759ef84cf
xItsSunny <luk.kliem@gmail.com>
2025-01-20 16:24:38 +0100

    release 3.0.2.1

--- src/main/java/keystrokesmod/Const.java
+++ src/main/java/keystrokesmod/Const.java
@@ -9,10 +9,8 @@ import java.util.List;
 @Unmodifiable
 public final class Const {
     public static final String NAME = "Raven XD";
-    public static final String VERSION = "3.0.2";
+    public static final String VERSION = "3.0.2.1";
     public static final List<String> CHANGELOG = Collections.unmodifiableList(Arrays.asList(
-            "[+] **Re-Add** Ability to choose Anticheat checks",
-            "[+] Add E-Sound Cape",
-            "[-] Remove Raven-XD mode from ClientSpoofer"
+            "-[※] **Fix** Ability to choose Anticheat checks"
     ));
 }

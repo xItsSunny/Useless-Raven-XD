ef4a5c2f01825527bccaa1bfa58f3e2f4a71e751
xItsSunny <luk.kliem@gmail.com>
2025-01-20 14:58:48 +0100

    3.0.2 update

--- src/main/java/keystrokesmod/Const.java
+++ src/main/java/keystrokesmod/Const.java
@@ -9,15 +9,10 @@ import java.util.List;
 @Unmodifiable
 public final class Const {
     public static final String NAME = "Raven XD";
-    public static final String VERSION = "3.0.1";
+    public static final String VERSION = "3.0.2";
     public static final List<String> CHANGELOG = Collections.unmodifiableList(Arrays.asList(
-            "[=] **Fix** backwards scaffold rotation snap with movefix.",
-            "[=] **Fix** Hypixel scaffold rotation sometimes fail.",
-            "[=] **Fix** Trajectories sometimes fail to predict.",
-            "[=] **Fix** CustomFOV broken.",
-            "[=] **Fix** AutoPlace broken.",
-            "[=] **Fix** Ambience broken.",
-            "[=] **Fix** Chams broken.",
-            "[-] **Remove** AutoChest."
+            "[+] **Re-Add** Ability to choose Anticheat checks",
+            "[+] Add E-Sound Cape",
+            "[-] Remove Raven-XD mode from ClientSpoofer"
     ));
 }

480721b5b986753a5dfa54cfef307df84ccc10da
CanCuocTinhYeu <137249503+EmDunjgbuon@users.noreply.github.com>
2025-02-17 22:50:27 +0700

    d

--- src/main/java/keystrokesmod/Const.java
+++ src/main/java/keystrokesmod/Const.java
@@ -9,8 +9,8 @@ import java.util.List;
 @Unmodifiable
 public final class Const {
     public static final String NAME = "Raven XD";
-    public static final String VERSION = "3.1 DEVDEV";
+    public static final String VERSION = "3.1";
     public static final List<String> CHANGELOG = Collections.unmodifiableList(Arrays.asList(
-            "-[Add] intaveSpeed "
+            "[Add] intaveSpeed "
     ));
 }
